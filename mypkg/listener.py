79b1f3abe76463f9579190c93f3b2ee7258e8add
Yodai Shiota <shioyan0809@gmail.com>
2023-11-23 21:53:06 +0900

    add service in listener

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,12 +1,33 @@
 import rclpy
 from rclpy.node import Node
-from person_msgs.msg import Person
+from person_msgs.srv import Query
 
-def cb(msg):
-    global node
-    node.get_logger().info("Listen: %d" % msg)
+def main():
+    rclpy.init()
+    node = Node("listener")
+    client = node.create_client(Query, 'query')
+    while not client.wait_for_service(timeout_sec=1.0):
+        node.get_logger().info('待機中')
 
-rclpy.init()
-node = Node("listener")
-pub = node.create_subscription(Person, "person", cb, 10)
-rclpy.spin(node)
+    req = Query.Request()
+    req.name = "塩田陽大"
+    future = client.call_async(req)
+
+    while rclpy.ok():
+        rclpy.spin_once(node)
+        if future.done():
+            try:
+                response = future.result()
+            except:
+                node.get_logger().info('呼び出し失敗')
+            else:
+                node.get_logger().info("age: {}".format(response.age))
+
+            break
+
+
+    node.destroy_node()
+    rclpy.shutdown()
+
+if ＿name＿ == '＿main＿':
+    main()
